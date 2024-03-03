0038e35374b00aaf573c45c3e764b41c5fbbb761
Meet Sable <meetsable@gmail.com>
2024-03-02 19:24:33 -0500

    minor

--- lua/settings/colorscheme.lua
+++ lua/settings/colorscheme.lua
@@ -1,9 +1,7 @@
 -- Themery block
 -- This block will be replaced by Themery.
 vim.opt.background = "dark"
-vim.cmd("let g:gruvbox_contrast_dark='medium'")
 
-
-vim.cmd("colorscheme gruvbox")
-vim.g.theme_id = 3
+vim.cmd("colorscheme onedark")
+vim.g.theme_id = 7
 -- end themery block
